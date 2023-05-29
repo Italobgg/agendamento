971962b0363ba0b070f008ffd978efbb9487e4ba
Italo Bartolomeu <italobggdev@gmail.com>
2023-05-29 16:05:43 -0300

    pag agendamento ok

---
+++ assets/script/agendamentoConcluido.js
@@ -0,0 +1,6 @@
+function agendamentoConcluido(event) {
+    event.preventDefault(); // Impede o envio do formulário padrão
+  
+    // Realiza o redirecionamento para a página desejada
+    window.location.href = "agendamento-concluido.html";
+  }
